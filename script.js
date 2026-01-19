9a438b55a050f8dd12cc697f000d037685bfd41f
omulll <gangsan1123gmail.com>
2026-01-19 03:45:13 +0000

    Create function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+//Randomly returns rock, paper, or scissors
+function getComputerChoice() {
+    let rpsValue = Math.trunc(Math.random() * 3);
+
+    switch (rpsValue) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
+
+
+console.log(getComputerChoice());
